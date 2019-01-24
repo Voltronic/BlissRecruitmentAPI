04169a47ac323e97e032d3aab1727536cba911fc
Voltronic <46977552+Voltronic@users.noreply.github.com>
2019-01-24 01:38:56 +0000

    Added Health Controller and Enabled Cors

--- BlissRecruitmentAPI/App_Start/WebApiConfig.cs
+++ BlissRecruitmentAPI/App_Start/WebApiConfig.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace BlissRecruitmentAPI
 {
@@ -10,15 +13,19 @@ namespace BlissRecruitmentAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
-                name: "BlissRecruitmentAPI",
-                routeTemplate: "{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                name: "BlissRecruitmentAPIRoute",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { action= "Get", id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
---
+++ BlissRecruitmentAPI/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace BlissRecruitmentAPI.Controllers
+{
+    /// <summary>
+    /// BlissRecruitmentAPI Health controller
+    /// </summary>
+    public class HealthController : ApiController
+    {
+        // GET health
+        public HttpResponseMessage Get()
+        {
+            var health = new ServerHealth();
+            health.status = HttpStatusCode.OK.ToString();
+
+            return Request.CreateResponse(HttpStatusCode.OK, health, Configuration.Formatters.JsonFormatter);
+        }
+    }
+}
